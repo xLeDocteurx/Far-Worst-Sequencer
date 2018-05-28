1ea2e4e16cc3ccfcea9ea6969df87a5cffa0c9ab
xLeDocteurx <lenoirc.fr@gmail.com>
2018-05-28 17:26:46 +0200

    finished a raw integration of the MaKey

--- js/eventListener.js
+++ js/eventListener.js
@@ -1,23 +1,28 @@
-var makeyTrack = all_tracks[0];
-console.table(makeyTrack);
+// var makeyTrack = all_tracks[0];
+// console.table(makeyTrack);
 
 document.addEventListener('keypress', (event) => {
     const keyName = event.key;
     console.log(keyName);
     if (keyName == "ArrowLeft") {
         console.log("Key pressed" + keyName);
-        all_tracks[0].channels[0].sample.play();
+        // all_tracks[0].channels[0].sample.play();
+        wiked.play();
     } else if (keyName == "ArrowRight") {
         console.log("Key pressed" + keyName);
-        all_tracks[0].channels[1].sample.play();
+        // all_tracks[0].channels[1].sample.play();
+        jungleismassive.play();
     } else if (keyName == "ArrowUp") {
         console.log("Key pressed" + keyName);
-        all_tracks[0].channels[2].sample.play();
+        // all_tracks[0].channels[2].sample.play();
+        ting.play();
     } else if (keyName == "ArrowDown") {
         console.log("Key pressed" + keyName);
-        all_tracks[0].channels[3].sample.play();
+        // all_tracks[0].channels[3].sample.play();
+        yo.playMode("restart");
+        yo.play();
     } else if (keyName == " ") {
         console.log("Key pressed" + keyName);
-        all_tracks[0].channels[4].sample.play();
+        // all_tracks[0].channels[4].sample.play();
     }
 });
